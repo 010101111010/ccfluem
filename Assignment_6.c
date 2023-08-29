894deef98a82a0d241fdffce7365beae04c903fa
ccfluem <142495440+ccfluem@users.noreply.github.com>
2023-08-30 01:20:07 +0530

    Update Assignment_6.c

--- Assignment_6.c
+++ Assignment_6.c
@@ -61,3 +61,89 @@ int main(){
 
     return 0;
 }
+
+
+
+
+//second question------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node * next;
+};
+struct node * head = NULL,* newnode ,* temp;
+
+
+//To enter a data given location
+void insert_given_position(){
+    int pos; int i=1;
+    printf("Enter your position : ");
+    scanf("%d",&pos);
+    int count;
+    temp = head;
+    while(temp != 0){
+        count++;
+        temp=temp->next;
+    }
+    if(pos>count){
+        printf("Invalid Input \n");
+    }
+    else{
+        newnode=(struct node *)malloc(sizeof(struct node));
+        temp = head;
+        while(i<pos-1){
+            temp=temp->next;
+            i++;
+        }
+        printf("Enter your New data : ");
+        scanf("%d",&newnode->data);
+        newnode -> next =NULL;
+        newnode ->next = temp ->next;
+        temp ->next = newnode;
+    }
+}
+
+//To crate linked list
+void createNew(int n){
+    for(int i = 1 ; i <= n ; i++){
+        newnode = (struct node * )malloc(sizeof(struct node));
+        printf("Enter your data : ");
+        scanf("%d",&newnode -> data);
+        newnode -> next = NULL;
+        if(head == NULL){
+            head = temp = newnode;
+        }
+        else {
+            temp -> next = newnode ;
+            temp = newnode;
+        }
+    }
+}
+
+//To print the linkedlist
+void display(){
+    temp = head;
+    while(temp != NULL){
+        printf("%d ",temp -> data);
+        temp = temp -> next;
+    }
+}
+
+//Main program 
+int main(){
+    int n;
+    printf("Enter your Linked List size : ");
+    scanf("%d",&n);
+    createNew(n);
+    printf("Your linked list is : \n");
+    display();
+    printf("\n");
+    insert_given_position();
+    printf("Your final Linked List is : \n");
+    display();
+
+    return 0;
+}
